d07edf6d04f63b2fd95dd8b34a4cb7aa1f4c7299
hongki09 <73209580+hongki09@users.noreply.github.com>
2020-11-01 02:33:02 +0700

    Add files via upload
    first commit

---
+++ PBO1010119915Latihan18/src/pbo1010119915latihan18/PBO1010119915Latihan18.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1010119915latihan18;
+
+public class PBO1010119915Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float kelipatan;
+        for(kelipatan =(float)3.5;kelipatan<=35.0;kelipatan+=3.5){
+            System.out.println(kelipatan);
+
+        }
+    }
+    
+}
